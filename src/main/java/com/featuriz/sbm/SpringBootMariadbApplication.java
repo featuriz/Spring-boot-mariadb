42524d654e9ac637d5e3e5dfcc9c93b2381e96db
sudhakar <featuriz@gmail.com>
2021-11-17 21:50:56 +0530

    Creating New baranch and Adding Author Info

--- src/main/java/com/featuriz/sbm/SpringBootMariadbApplication.java
+++ src/main/java/com/featuriz/sbm/SpringBootMariadbApplication.java
@@ -3,6 +3,8 @@ package com.featuriz.sbm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Sudhakar Krishnan <featuriz@gamil.com>
+
 @SpringBootApplication
 public class SpringBootMariadbApplication {
 
